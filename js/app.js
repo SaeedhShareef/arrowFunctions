5414342d3ff851519c14b87f4ec49ed8d09daf19
Saeed Shareef <saeedhshareef@gmail.com>
2020-07-10 23:09:44 -0400

    arrowFunctions

---
+++ js/app.js
@@ -0,0 +1,42 @@
+console.log("ay yo we gud?")
+
+var a = 10
+var b = 20
+console.log(a)
+
+ function newSum (argumentOne, argumentTwo) {
+    return (argumentOne + argumentTwo)
+ }
+ console.log(newSum(a,b))
+
+function remainder(argumentOne, argumentTwo) {
+        return (argumentOne % argumentTwo)
+
+}
+        console.log(remainder(a,b))
+
+        function multiply( argumentOne, argumentTwo){
+                return (argumentOne * argumentTwo)
+        }
+        console.log(multiply(a,b))
+
+         function divide(argumentOne, argumentTwo){
+                    return (argumentOne / argumentTwo)
+         }
+         console.log(divide(a,b))
+
+       newSum = (a,b) => a + b;
+
+       console.log(newSum(a,b))
+
+       remainder = (a,b) => a % b ;
+
+       console.log(remainder(a,b))
+
+       multiply = (a,b) => a * b ;
+
+       console.log(multiply(a,b))
+      
+       divide = (a,b) => a / b ;
+
+       console.log(divide(a,b))
